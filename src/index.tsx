b0fb0d27e2ba9c57ce10b2165893938ce6db5490
Hadi Azarnasab <hazard7546@gmail.com>
2023-09-22 02:48:01 +0330

    Deploy branch

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import "./i18n";
-import "./index.scss";
-import App from "./App";
-
-const createRoot = require("react-dom/client").createRoot;
-
-const container = document.getElementById("root");
-const root = createRoot(container!);
-root.render(<App />);
